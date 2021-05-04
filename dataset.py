1a1dc71a4321c9ba5b72e88e5394d6641ee82b67
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2021-05-04 12:14:10 +0200

    Fix typo on dataset.py

--- dataset.py
+++ dataset.py
@@ -245,7 +245,7 @@ class VocalTractMaskRCNNDataset(Dataset):
         g_arr = self.to_tensor(g)
 
         r = self.read_dcm(R_filepath, "L") if R_filepath is not None else None
-        r_arr = self.to_tensor(r) if r is not None else g_rr
+        r_arr = self.to_tensor(r) if r is not None else g_arr
 
         b = self.read_dcm(B_filepath, "L") if B_filepath is not None else None
         b_arr = self.to_tensor(b) if b is not None else g_arr
