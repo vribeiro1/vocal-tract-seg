7f03f59f93d0344068d715817fd4ac2883b2d6df
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2022-05-31 13:43:18 +0200

    Include DatasetConfig

--- settings.py
+++ settings.py
@@ -6,3 +6,20 @@ ARTSPEECH_DIR = os.path.join(BASE_DIR, "data_ArtSpeech", "training")
 TRAIN = "train"
 VALID = "validation"
 TEST = "test"
+
+
+class ArtSpeechConfig:
+    RES = 136
+    PIXEL_SPACING = 1.6176470518112
+    FRAMERATE = 50
+    SYNC_SHIFT = 2
+
+
+class GottingenConfig:
+    RES = 136
+    PIXEL_SPACING = 1.4117647409439
+    FRAMERATE = 55
+    SYNC_SHIFT = 0
+
+
+DatasetConfig = ArtSpeechConfig
